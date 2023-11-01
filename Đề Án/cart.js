b54d9ecdf4a4ffe2e87c0e71905b1df8ab39686f
Dương Duy Khiêm <93518433+KhiemDuong2405@users.noreply.github.com>
2023-11-01 08:36:46 +0700

    Add files via upload

--- Đề Án/cart.js
+++ Đề Án/cart.js
@@ -27,4 +27,39 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-
+document.addEventListener('DOMContentLoaded', function(){
+    const tang = document.querySelector('.cart-add');
+    const giam = document.querySelector('.cart-reduce');
+    const productquantify= document.querySelector('.product-quantity');
+    const priceproduct = document.querySelector('.price-product-cart');
+    const productquantify2= document.querySelector('.allprice');
+    const totalallprice = document.querySelector('.totalprice');
+    const moneyall=document.querySelector('.money-all');
+    const moneypoint=document.querySelector('.money-point');
+    let soluong=1;
+    tang.addEventListener("click", function () {
+        soluong+=1;
+        productquantify.textContent = soluong;
+        productquantify2.textContent = soluong; // Cập nhật giá trị số lượng sản phẩm trên trang
+        capNhatTongGiaTien(); 
+      });
+    giam.addEventListener("click", function () {
+        soluong-=1;
+        if(soluong<=0)
+        soluong=0;
+        productquantify.textContent = soluong;
+        productquantify2.textContent = soluong; // Cập nhật giá trị số lượng sản phẩm trên trang
+        
+        capNhatTongGiaTien();
+      });
+    function layGiaTien() {
+        return parseInt(priceproduct.textContent, 10);
+      }
+      function capNhatTongGiaTien() {
+        const giaTien = layGiaTien();
+        const tongGiaTien = giaTien * soluong;
+        moneyall.textContent= tongGiaTien;
+        moneypoint.textContent=tongGiaTien/1000;
+        totalallprice.textContent = tongGiaTien ;
+    }
+})
