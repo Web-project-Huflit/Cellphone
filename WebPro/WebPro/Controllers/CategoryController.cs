b2f61247efcece5d404b8a022ef668cea2ea042c
vutran1203 <144185881+vutran1203@users.noreply.github.com>
2023-11-14 21:40:55 +0700

    Add files via upload

---
+++ WebPro/WebPro/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Đồ_án.Models;
+namespace Đồ_án.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ILogger<CategoryController> _logger1;
+
+        public CategoryController(ILogger<CategoryController> logger1)
+        {
+            _logger1 = logger1;
+        }
+
+        public IActionResult Phones()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebPro/WebPro/Controllers/MobileController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Đồ_án.Controllers
+{
+    public class MobileController : Controller
+    {
+        public IActionResult Phones()
+        {
+            return View();
+        }
+        
+
+    }
+}
